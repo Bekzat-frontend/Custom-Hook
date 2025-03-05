7d838a7999e23f1db76f003b66fabcb38c8c9810
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-05 23:16:06 +0700

    lkfuisknc

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-// import React from "react";
-// import useCounter from "./assets/Button";
+import React from "react";
+import useCounter from "./assets/Button";
 
-// function App() {
-//   const [priverder, plus, minus] = useCounter();
+function App() {
+  const [priverder, plus, minus] = useCounter();
 
-//   const [priverder1, plus1, minus1] = useCounter();
+  const [priverder1, plus1, minus1] = useCounter();
 
-//   return (
-//     <div>
-//       <button onClick={plus}>+</button>
-//       <h2>{priverder}</h2>
-//       <button onClick={minus}>-</button>
-//       <button onClick={plus1}>+</button>
-//       <h2>{priverder1}</h2>
-//       <button onClick={minus1}>-</button>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <button onClick={plus}>+</button>
+      <h2>{priverder}</h2>
+      <button onClick={minus}>-</button>
+      <button onClick={plus1}>+</button>
+      <h2>{priverder1}</h2>
+      <button onClick={minus1}>-</button>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
