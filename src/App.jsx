41e2befb1e247bf53f0da073c2bdd5daadf8af9e
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-05 22:47:23 +0700

    dal

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import React from "react";
-import useCounter from "./assets/Button";
+// import React from "react";
+// import useCounter from "./assets/Button";
+
+// function App() {
+//   const [priverder, plus, minus] = useCounter();
+
+//   const [priverder1, plus1, minus1] = useCounter();
+
+//   return (
+//     <div>
+//       <button onClick={plus}>+</button>
+//       <h2>{priverder}</h2>
+//       <button onClick={minus}>-</button>
+//       <button onClick={plus1}>+</button>
+//       <h2>{priverder1}</h2>
+//       <button onClick={minus1}>-</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 
 function App() {
-  const [priverder, plus, minus] = useCounter();
-
-  const [priverder1, plus1, minus1] = useCounter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div>
-      <button onClick={plus}>+</button>
-      <h2>{priverder}</h2>
-      <button onClick={minus}>-</button>
-      <button onClick={plus1}>+</button>
-      <h2>{priverder1}</h2>
-      <button onClick={minus1}>-</button>
+      {isLoggedIn ? (
+        <h1>Кош келди, колдонуучу!</h1>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Кирүү</button>
+      )}
     </div>
   );
 }
 
 export default App;
-
